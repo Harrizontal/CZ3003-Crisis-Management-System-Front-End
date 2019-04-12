29490b99c142aefe396b87f602a7b93973fa37f5
tccl9438 <44692141+tccl9438@users.noreply.github.com>
2019-04-12 11:57:22 +0800

    Updated with "NotFound" page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,13 +1,28 @@
-import React from 'react';
-
-export default () => {
-  return (
-    <body className="bg404">
-      <div>
-      <div className="e404">OOPS!</div>
-        <p className="message">Sorry, that page does not exist.</p>
-      </div>
-    </body>
-
-  );
-};
+import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+class NotFound extends Component{
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.history.goBack();
+  }
+  render() {
+    return (
+      <div className="bg404">
+        <div className="containerNF">
+          <div className="e404">The page you were looking for doesn't exist</div>
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className="buttonNF"
+            onClick={e => this.onSubmit(e)}
+            >
+            Click here to return
+          </Button>
+        </div>      
+      </div>     
+    );
+  };
+}
+export default (NotFound);
+  
