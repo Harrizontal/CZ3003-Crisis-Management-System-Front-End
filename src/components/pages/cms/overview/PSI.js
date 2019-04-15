131b1f26b1992a43f45401473a24e9ed79868f97
Guang Yuan Tay <guangyuantay@gmail.com>
2019-04-15 21:49:21 +0800

    1. Replaced fav.ico

--- src/components/pages/cms/overview/PSI.js
+++ src/components/pages/cms/overview/PSI.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 export default class PSI extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div 
         style={{        
         zIndex: 2,
@@ -58,74 +57,6 @@ export default class PSI extends Component {
       </div>
       </div>
       
-=======
-      <React.Fragment>
-        <div
-          style={{
-            zIndex: 2,
-            position: "absolute",
-            right: 0,
-            bottom: "10px"
-          }}
-          className="psi-card"
-        />
-        <div
-          className="containerLegendPSI"
-          style={{
-            zIndex: 2,
-            position: "absolute",
-            right: 0,
-            bottom: "300px"
-          }}
-        >
-          <table className="legendPSI">
-            <tr>
-              <th rowSpan="2">Area</th>
-              <th rowSpan="2">PSI</th>
-              <th className="aq">Quality</th>
-            </tr>
-            <tr>
-              <th>Descriptor</th>
-            </tr>
-            <tr>
-              <td>North</td>
-              <td>50</td>
-              <td>
-                <div className="green" />
-              </td>
-            </tr>
-            <tr>
-              <td>South</td>
-              <td>44</td>
-              <td>
-                <div className="green" />
-              </td>
-            </tr>
-            <tr>
-              <td>Central</td>
-              <td>43</td>
-              <td>
-                <div className="green" />
-              </td>
-            </tr>
-            <tr>
-              <td>East</td>
-              <td>41</td>
-              <td>
-                <div className="green" />
-              </td>
-            </tr>
-            <tr>
-              <td>West</td>
-              <td>43</td>
-              <td>
-                <div className="green" />
-              </td>
-            </tr>
-          </table>
-        </div>
-      </React.Fragment>
->>>>>>> 15157e41e575a6909711d94c211599dce3f40213
     );
   }
 }
