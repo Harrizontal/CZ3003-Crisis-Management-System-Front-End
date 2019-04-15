ebb339817b0a9030022003a827eb8c8274220ffd
tccl9438 <44692141+tccl9438@users.noreply.github.com>
2019-04-15 20:22:00 +0800

    PSI legend

--- src/components/pages/cms/overview/PSI.js
+++ src/components/pages/cms/overview/PSI.js
@@ -2,6 +2,44 @@ import React, { Component } from "react";
 
 export default class PSI extends Component {
   render() {
-    return <div>PSI Information. here</div>;
+    return (
+      <div className= "containerLegendPSI">
+        <br/>
+        <table className="legendPSI">
+          <tr>
+            <th rowSpan="2">Area</th>
+            <th rowSpan="2">PSI value</th>
+            <th className="aq">Air Quality</th>
+          </tr>
+          <tr><th>Descriptor</th></tr>
+          <tr>
+            <td>North</td>
+            <td>50</td>
+            <td><div className="green"></div></td>
+          </tr>
+          <tr>
+            <td>South</td>
+            <td>44</td>
+            <td><div className="green"></div></td>
+          </tr>
+          <tr>
+            <td>Central</td>
+            <td>43</td>
+            <td><div className="green"></div></td>
+          </tr>
+          <tr>
+            <td>East</td>
+            <td>41</td>
+            <td><div className="green"></div></td>
+          </tr>
+          <tr>
+            <td>West</td>
+            <td>43</td>
+            <td><div className="green"></div></td>
+          </tr>
+        </table>
+      </div>
+    );
+
   }
 }
