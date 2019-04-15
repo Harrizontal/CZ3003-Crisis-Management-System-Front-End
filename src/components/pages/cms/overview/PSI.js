15157e41e575a6909711d94c211599dce3f40213
Harrison Wong Jun Yong <Harrisonwjy@hotmail.com>
2019-04-15 21:32:36 +0800

    Fixed some styling at PSI

--- src/components/pages/cms/overview/PSI.js
+++ src/components/pages/cms/overview/PSI.js
@@ -1,63 +1,74 @@
 import React, { Component } from "react";
 
 export default class PSI extends Component {
-
   render() {
     return (
-      <div 
-        style={{        
-        zIndex: 2,
-        position: "absolute",
-        right: "40px",
-        bottom: "30px"
-        }}
-        className="psi-card">
-         
-      
-      <div className= "containerLegendPSI"
-      style = {{
-        zIndex: 2,
-        position: "absolute",
-        right: "0px",
-        bottom: "330px"
-      }}
-      >
-        <table className="legendPSI">
-          <tr>
-            <th rowSpan="2">Area</th>
-            <th rowSpan="2">PSI value</th>
-            <th className="aq">Air Quality</th>
-          </tr>
-          <tr><th>Descriptor</th></tr>
-          <tr>
-            <td>North</td>
-            <td>50</td>
-            <td><div className="green"></div></td>
-          </tr>
-          <tr>
-            <td>South</td>
-            <td>44</td>
-            <td><div className="green"></div></td>
-          </tr>
-          <tr>
-            <td>Central</td>
-            <td>43</td>
-            <td><div className="green"></div></td>
-          </tr>
-          <tr>
-            <td>East</td>
-            <td>41</td>
-            <td><div className="green"></div></td>
-          </tr>
-          <tr>
-            <td>West</td>
-            <td>43</td>
-            <td><div className="green"></div></td>
-          </tr>
-        </table>
-      </div>
-      </div>
-      
+      <React.Fragment>
+        <div
+          style={{
+            zIndex: 2,
+            position: "absolute",
+            right: 0,
+            bottom: "10px"
+          }}
+          className="psi-card"
+        />
+        <div
+          className="containerLegendPSI"
+          style={{
+            zIndex: 2,
+            position: "absolute",
+            right: 0,
+            bottom: "300px"
+          }}
+        >
+          <table className="legendPSI">
+            <tr>
+              <th rowSpan="2">Area</th>
+              <th rowSpan="2">PSI</th>
+              <th className="aq">Quality</th>
+            </tr>
+            <tr>
+              <th>Descriptor</th>
+            </tr>
+            <tr>
+              <td>North</td>
+              <td>50</td>
+              <td>
+                <div className="green" />
+              </td>
+            </tr>
+            <tr>
+              <td>South</td>
+              <td>44</td>
+              <td>
+                <div className="green" />
+              </td>
+            </tr>
+            <tr>
+              <td>Central</td>
+              <td>43</td>
+              <td>
+                <div className="green" />
+              </td>
+            </tr>
+            <tr>
+              <td>East</td>
+              <td>41</td>
+              <td>
+                <div className="green" />
+              </td>
+            </tr>
+            <tr>
+              <td>West</td>
+              <td>43</td>
+              <td>
+                <div className="green" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </React.Fragment>
     );
   }
 }
